49fd2dbad9a8bf3c63f30a2c16def13d6b321699
leticiagc <leticia.calixto@ccc.ufcg.edu.br>
2025-03-19 14:50:08 -0300

    feat: initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './services/api.service'
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports:[RouterOutlet]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  usuarios: any[] = [];
+  projetos: any[] = [];
+
   title = 'exemplo';
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.apiService.getUsuarios().subscribe((res) => {
+      this.usuarios = res;
+      console.log('Usuários:', this.usuarios);
+    });
+
+    this.apiService.getProjetos().subscribe((res) => {
+      this.projetos = res;
+      console.log('Projetos:', this.projetos);
+    });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient()
+  ],
+})
   .catch((err) => console.error(err));
