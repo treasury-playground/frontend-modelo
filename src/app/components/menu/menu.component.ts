a45d07dd47d910d3490333f588bcefcdb657fc28
pedroarthurob <pedro.arthur.barreto@ccc.ufcg.edu.br>
2025-04-13 22:08:32 -0300

    feat: menu do professor dinâmico de acordo com os projetos presentes na db

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { CommonModule } from '@angular/common';
+
+interface Project {
+  id: number;
+  name: string;
+  description: string;
+  date: string;
+  coordinatorId: number;
+  students: Array<{ id: number; role: string }>;
+}
 
 @Component({
   selector: 'app-menu',
-  imports: [],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+  styleUrls: ['./menu.component.css'],
+  imports: [CommonModule]
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  projects: Project[] = [];
 
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getProjetos().subscribe((projects: Project[]) => {
+      this.projects = projects;
+    });
+  }
 }
