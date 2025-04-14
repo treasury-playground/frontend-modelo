d00444bb8a6d3f9b94d83984f607fbdfc616d637
pedroarthurob <pedro.arthur.barreto@ccc.ufcg.edu.br>
2025-04-13 22:16:31 -0300

    feat: adiciona chamadas http para ser utilizadas nos componentes

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,6 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+export interface User {
+  id: number;
+  fullName: string;
+  email: string;
+  role: string;
+  educationalInstitution: string; 
+}
+
+export interface Project {
+  id: number;
+  name: string;
+  description: string;
+  date: string;
+  coordinatorId: number;
+  students: Array<{ id: number; role: string }>;
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,6 +27,7 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
+  // GET
   getUsuarios(): Observable<any> {
     return this.http.get(`${this.baseUrl}/users`);
   }
@@ -18,4 +36,19 @@ export class ApiService {
     return this.http.get(`${this.baseUrl}/projects`);
   }
   
+  // POST
+  createProject(project: Project): Observable<Project> {
+    return this.http.post<Project>(`${this.baseUrl}/projects`, project);
+  }
+
+  // PUT
+  updateProject(project: Project): Observable<Project> {
+    return this.http.put<Project>(`${this.baseUrl}/projects/${project.id}`, project);
+  }
+
+  // DELETE
+  deleteProject(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/projects/${id}`);
+  }
+
 }
