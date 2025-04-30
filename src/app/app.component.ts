090d22daf4db3305f114c763a3a6daeb95c77b82
pedroarthurob <pedro.arthur.barreto@ccc.ufcg.edu.br>
2025-04-30 01:00:16 -0300

    chore: apaga código não utilizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from './services/api.service'
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,23 +7,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.css'],
   imports: [RouterOutlet]
 })
-export class AppComponent implements OnInit {
-  usuarios: any[] = [];
-  projetos: any[] = [];
-
-  title = 'exemplo';
-
-  constructor(private apiService: ApiService) { }
-
-  ngOnInit(): void {
-    this.apiService.getUsuarios().subscribe((res) => {
-      this.usuarios = res;
-      console.log('Usuários:', this.usuarios);
-    });
-
-    this.apiService.getProjetos().subscribe((res) => {
-      this.projetos = res;
-      console.log('Projetos:', this.projetos);
-    });
-  }
+export class AppComponent {
 }
